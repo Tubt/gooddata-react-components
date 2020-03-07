091b5e4416e9f95593edc2e3fc26c8b90259b5e3
Tubt <tu.bui@gooddata.com>
2020-03-07 21:17:25 +0700

    change server

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = async (env, argv) => {
     const proxy = {
         "/gdc": {
             changeOrigin: true,
-            cookieDomainRewrite: "10.0.69.67",
+            cookieDomainRewrite: "localhost",
             secure: false,
             target: backendUrl,
             headers: {
@@ -56,7 +56,7 @@ module.exports = async (env, argv) => {
             },
         },
         "/api": {
-            target: "http://10.0.69.67:8880",
+            target: "http://localhost:8999",
             secure: false,
             onProxyReq: req => {
                 console.log("proxy", "/gdc", req.path); // eslint-disable-line no-console
@@ -65,7 +65,7 @@ module.exports = async (env, argv) => {
                     req.setHeader("content-length", "0");
                 }
                 // eslint-disable-next-line no-console
-                console.log(`Proxy ${req.path} to http://10.0.69.67:8880 (use: yarn examples-server)`);
+                console.log(`Proxy ${req.path} to http://localhost:8999 (use: yarn examples-server)`);
             },
         },
     };
